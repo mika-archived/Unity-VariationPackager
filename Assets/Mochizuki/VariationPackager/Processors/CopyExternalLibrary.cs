1c7304fb2ec3253b56c1905501b20435c5a2c70d
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-06-30 19:57:40 +0900

    feat: add preprocessor for build task

---
+++ Assets/Mochizuki/VariationPackager/Processors/CopyExternalLibrary.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using Mochizuki.VariationPackager.Models.Abstractions;
+
+using UnityEngine;
+
+namespace Mochizuki.VariationPackager.Processors
+{
+    public class CopyExternalLibrary : Processor
+    {
+        public override void Run()
+        {
+            var externals = Path.Combine(Application.dataPath, "..", "src", "VariationPackager", "bin", "Release");
+            var destination = Path.Combine(Application.dataPath, "Mochizuki", "VariationPackager", "Plugins");
+            var libraries = new List<string>
+            {
+                "Microsoft.Extensions.FileSystemGlobbing.dll",
+                "Newtonsoft.Json.dll",
+                "VariationPackager.dll"
+            };
+
+            foreach (var library in libraries)
+                File.Copy(Path.Combine(externals, library), Path.Combine(destination, library), true);
+        }
+    }
+}
